7e477f6c1b8da27f97ef657fb23186a186d2284c
cassiaavila <laercio.aaavila@gmail.com>
2022-10-12 13:20:47 -0300

    Initial commit

---
+++ src/test/java/br/com/laercio/an/clan/ClanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.laercio.an.clan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
